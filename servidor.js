bdf7966e612bdc04ce5bcab27d5e517f67a5ae37
Bairon-page <bairon9239@gmail.com>
2025-07-07 17:53:04 -0500

    subirtodo

--- servidor.js
+++ servidor.js
@@ -1,60 +1,70 @@
 let express = require('express');
 let app = express();
-let fs = require('fs');
-let {exec} = require('chlid_process');
-const cors = require('cors');
-app.use(cors({
-  origin: '*', // Reemplaza con tu dominio
-  methods: ['POST', 'GET']
-}));
+let path = require('path');
+let { createClient } = require('@supabase/supabase-js') ;
+
+
+app.use(express.static(path.join(__dirname+'/www'),{index:'index.html'}));
+
+app.use(express.text({type:'text/plain'}))
+
+
+let url = 'https://gnaqwcyeeqqkqwxcxbcg.supabase.co';
+
+let key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImduYXF3Y3llZXFxa3F3eGN4YmNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4NTcyOTksImV4cCI6MjA2NzQzMzI5OX0.hvw2ACL-3a7EGvFH1XRvlYu77IGSjB1uOvY94nJXu28';
+
+
+
+let cliente = createClient(url,key);
 
 
 
 
 
-app.use(express.static(__dirname));
-app.use(express.text({type:'text/plain'}));
-
-app.get('/', (req,res) => {
-    
-    
-    res.sendFile(__dirname+'/index.html');
-    
-    
-    
-    
-    
-});
-
-app.get('/pedirar', (req,res) => {
-exec('cat archivo.txt',(_,r) => { console.log(r)});
-
-
-});
 
 app.post('/enviar', (req,res) => {
     
-    fs.appendFile(`archivo.txt`,req.body+'\n',(err) => {
+    let mensaje = req.body;
+    
+    
+    cliente.from('datos').insert([{
         
-        if(err) {
-            console.log(err)
-        } else {
-            console.log(`mensaje: (${req.body}), guardado con exito✅`);
+       id: 333,
+       texto: mensaje 
+        
+        
+    }]).then(resp => {
+        
+        
+        if (!resp.error) {
             
-           
+            res.send('Enviado correctamente');
+            
+            
+        } else {
+            
+            res.send('no se pudo enviar');
+            
         }
         
         
     });
-     
-   res.send('enviado');
+    
+    
     
 });
 
 
-app.listen(3000,'0.0.0.0',() => {
+
+// si voy a escribir escuchando y me llama mucho la atencion algo que tenva que ver con una plabara que empieze por esc escribire esa palabra en vez de escuchando.
+
+app.listen(3000, () => {
     
     
-    console.log('escuchando puerto 3000');
+    console.log('escuchando puerto:3000')
+    
     
 });
+
+ 
+ 
